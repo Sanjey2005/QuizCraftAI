f77bacbb9fd726a834b9efc6b5760f9bfa1fe8d4
Sanjey <sanjey8105@gmail.com>
2026-03-15 11:59:19 +0530

    Update start.bat to use docker-compose up -d

--- start.bat
+++ start.bat
@@ -1,17 +1,15 @@
 @echo off
-echo Starting QuizCraft AI...
+echo Starting QuizCraft AI via Docker Compose...
 
-:: Django backend
-start "Django" cmd /k "cd /d %~dp0backend && python manage.py runserver"
-
-:: Celery worker (needed for AI quiz generation)
-start "Celery" cmd /k "cd /d %~dp0backend && celery -A config worker --loglevel=info"
-
-:: Next.js frontend
-start "Frontend" cmd /k "cd /d %~dp0frontend && npm run dev"
+:: Start all services defined in docker-compose.yml in detached mode
+docker-compose up -d
 
 echo.
-echo All services starting:
-echo   Django   ^>  http://localhost:8000
-echo   Frontend ^>  http://localhost:3000
+echo All services are starting in the background!
+echo   Django API ^>  http://localhost:8000
+echo   Frontend   ^>  http://localhost:3000
+echo.
+echo Note: If this is the first run, the frontend build might take a few minutes.
+echo To view live logs at any time, run: docker-compose logs -f
+echo To stop the application, run:       docker-compose down
 echo.
